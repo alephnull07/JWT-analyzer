fde9f5122b3a50494add990f3d170ecbbb2ae193
Aarav <68392333+BIGBRAINZZ@users.noreply.github.com>
2022-11-01 19:03:12 -0700

    Add files via upload

---
+++ JWT_analyzer.py
@@ -0,0 +1,27 @@
+import base64
+import binascii
+
+print("Welcome to the Cookie Debugger!")
+choice = input("Please enter the cookie type you want to analyze (JWT, base64_check")
+
+
+def JWT():
+    JWT = input("Enter your JSON Web Token here: ") + "."
+
+    split_arr = []
+    last_period = 0
+    for x in range(len(JWT)):
+        if JWT[x] == ".":
+            split_arr.append(JWT[last_period:x])
+            last_period = x + 1
+
+    header = base64.b64decode(split_arr[0] + '==').decode('utf-8')
+    payload = base64.b64decode(split_arr[1] + '==').decode('utf-8')
+    signature = split_arr[2]
+
+    print("Header: \n" + str(header))
+    print("Payload: \n" + str(payload))
+    print("Signature: \n" + str(signature))
+
+if choice == "JWT":
+    JWT()
